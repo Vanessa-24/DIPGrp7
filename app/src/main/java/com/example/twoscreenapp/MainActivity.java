125646f1a15063894378a86424a0545a56c4641d
leminhviett <minhviet810business@gmail.com>
2020-09-30 14:36:35 +0800

    nothing changed

--- app/src/main/java/com/example/twoscreenapp/MainActivity.java
+++ app/src/main/java/com/example/twoscreenapp/MainActivity.java
@@ -36,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
+//        if (android.os.Build.VERSION.SDK_INT > 9) {
+//            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
         mAuth = FirebaseAuth.getInstance();
         helloMsg = findViewById(R.id.helloMsg);
         ggSignIn = findViewById(R.id.googleSignIn);
