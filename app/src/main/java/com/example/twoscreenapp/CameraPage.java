5ed60f2d9fc0fef6a0dcd7584626a18b315aa7ee
ngkwangs <69684391+ngkwangs@users.noreply.github.com>
2020-10-21 16:00:54 +0800

    updated m25 model
    m25 build updated sfa sfb file

--- app/src/main/java/com/example/twoscreenapp/CameraPage.java
+++ app/src/main/java/com/example/twoscreenapp/CameraPage.java
@@ -172,7 +172,7 @@ public class CameraPage extends AppCompatActivity {
             // if doesn't fit any, maybe just use 1 default obj maybe...
             // and load model
             ModelRenderable.builder()
-                    .setSource(this, R.raw.m13)
+                    .setSource(this, R.raw.m25)
                     .build()
                     .thenAccept(renderable -> {
                         modelRenderable = renderable;
